4b729391832962eeaa64e8b14ac427df99e0326a
lihai <870721131@qq.com>
2016-11-09 18:05:28 +0800

    提交pom文件

---
+++ src/test/java/JunitTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2016/11/9.
+ */
+public class JunitTest {
+    @Test
+    public   void test(){
+        System.out.println("");
+    }
+}
